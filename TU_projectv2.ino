a47f2b5af7107afd799db2d6fcc97b8ef81e2a51
StanimirChilikov <154449685+StanimirChilikov@users.noreply.github.com>
2026-05-07 19:14:33 +0300

    Add files via upload

---
+++ TU_projectv2.ino
@@ -0,0 +1,156 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define Sone 2
+#define Stwo 3
+#define Sthree 4
+#define Sfour 11
+#define Sfive 12
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+const int pumpPin = 13;
+
+int ena = 5;
+int in1 = 6;
+int in2 = 7;
+int in3 = 8;
+int in4 = 9;
+int enb = 10;
+int trigPin = A0;
+int echoPin = A1;
+
+long duration;
+int distance;
+
+void setup() {
+
+  pinMode(Sone, INPUT);
+  pinMode(Stwo, INPUT);
+  pinMode(Sthree, INPUT);
+  pinMode(Sfour, INPUT);
+  pinMode(Sfive, INPUT);
+
+  pinMode(pumpPin, OUTPUT);
+
+  pinMode(ena, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enb, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  digitalWrite(pumpPin, LOW);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  Serial.begin(9600);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println("Starting...");
+  display.display();
+  delay(1000);
+}
+
+void loop() {
+
+  bool value1 = digitalRead(Sone);
+  bool value2 = digitalRead(Stwo);
+  bool value3 = digitalRead(Sthree);
+  bool value4 = digitalRead(Sfour);
+  bool value5 = digitalRead(Sfive);
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH, 30000);
+  distance = duration * 0.034 / 2;
+
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print("Distance: ");
+  display.print(distance);
+  display.println(" cm");
+  display.print("Flames: ");
+  display.print(digitalRead(Sone));
+  display.print(digitalRead(Stwo));
+  display.print(digitalRead(Sthree));
+  display.print(digitalRead(Sfour));
+  display.println(digitalRead(Sfive));
+  display.print("Pump: ");
+  if(digitalRead(pumpPin) == HIGH) {
+    display.println("ON");
+  } else {
+    display.println("OFF");
+  }
+  display.display();
+
+  /*Serial.print(value1);
+  Serial.print(" ");
+  Serial.print(value2);
+  Serial.print(" ");
+  Serial.print(value3);
+  Serial.print(" ");
+  Serial.print(value4);
+  Serial.print(" ");
+  Serial.println(value5);*/
+
+  /*if (distance < 50) {
+    Serial.println("Wall ahead, stopping!");
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+    digitalWrite(pumpPin, LOW);
+  } 
+  else*/ if (value3 == HIGH) {  // Fire ahead
+      Serial.println("Fire ahead");
+      digitalWrite(in1,HIGH);
+      digitalWrite(in2,LOW);
+      analogWrite(ena,255);
+      digitalWrite(in3,HIGH);
+      digitalWrite(in4,LOW);
+      analogWrite(enb,255);
+      digitalWrite(pumpPin,HIGH);
+  }
+  else if (value2 == HIGH || value1 == HIGH) {  // Fire right
+      Serial.println("Fire right");
+      digitalWrite(in1,HIGH);
+      digitalWrite(in2,LOW);
+      analogWrite(ena,255);
+      digitalWrite(in3,LOW);
+      digitalWrite(in4,LOW);
+      analogWrite(enb,0);
+      digitalWrite(pumpPin,LOW);
+  }
+  else if (value4 == HIGH || value5 == HIGH) {  // Fire left
+      Serial.println("Fire left");
+      digitalWrite(in1,LOW);
+      digitalWrite(in2,LOW);
+      analogWrite(ena,0);
+      digitalWrite(in3,HIGH);
+      digitalWrite(in4,LOW);
+      analogWrite(enb,255);
+      digitalWrite(pumpPin,LOW);
+  }
+  else {  // No fire
+      Serial.println("No fire");
+      digitalWrite(in1,LOW);
+      digitalWrite(in2,LOW);
+      digitalWrite(in3,LOW);
+      digitalWrite(in4,LOW);
+      digitalWrite(pumpPin,LOW);
+  }
+  delay(50);
+}
